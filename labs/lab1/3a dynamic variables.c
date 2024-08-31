597e42fac8116fa40a8c54ea0e756806bd55a4ac
Rachel <e0969575@u.nus.edu>
2024-08-31 20:48:45 +0800

    Create 3a dynamic variables.c

---
+++ labs/lab1/3a dynamic variables.c
@@ -0,0 +1,17 @@
+// Bring in the malloc and free function prototypes
+#include <stdlib.h>
+
+// Allocate memory to store an int
+z = (int *) malloc(sizeof(int))
+
+// The sizeof(.) function returns the number of bytes of the type specified as its argument. 
+// So sizeof(int) returns the number of bytes in an int.
+
+// The malloc(.) function takes one argument; the number of bytes to allocate. 
+// The malloc(.) function then returns a pointer to the memory that was allocated.
+  
+// The malloc(.) function’s return type is “void *”. W
+// While it seems strange to have a pointer to void, this is used in C to indicate a “generic” data type.
+
+// We want to assign the return pointer to a variable “z” of type int *. 
+// Thus we need to type-cast the “void *” return type of malloc(.) to int *.
